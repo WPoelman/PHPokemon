bd45243e522cdc602486287910b91b74684c3750
Robin van der Noord <robinvandernoord@gmail.com>
2019-05-28 20:19:04 +0200

    made menu more dynamic, added TODO to make it all even more dynamic

--- index.php
+++ index.php
@@ -11,27 +11,40 @@ include 'model.php';
 /* $right_column = use_template('cards'); */
 
 /* Set the default routes for the navigation bar */
-$navigation_tpl = Array(
-	0 => Array(
-		'name' => 'Home',
-		'url'  => 'index'
-	),
-	1 => Array(
-		'name' => 'Instructions',
-		'url'  => 'instructions'
-	),
-	2 => Array(
-		'name' => 'Pokemon',
-		'url'  => 'pokemon'
-	)
-);
+
+
+//$navigation_tpl = Array(
+//	0 => Array(
+//		'name' => 'Home',
+//		'url'  => 'index'
+//	),
+//	1 => Array(
+//		'name' => 'Instructions',
+//		'url'  => 'instructions'
+//	),
+//	2 => Array(
+//		'name' => 'Pokemon',
+//		'url'  => 'pokemon'
+//	)
+//);
+
+$routes = [
+	"home"         => new_route('/', 'get', 'Home'),
+	"instructions" => new_route('instructions', 'get', 'Instructions'),
+	"pokemon"      => new_route('pokemon', 'get', 'Pokemon'),
+	"handler"      => new_route('handler', 'post'),
+];
+
+
+// todo: translate to functions in stead of if elif
 
 /* Homepage */
-if (new_route('/', 'get')) {
+if ($routes['home']['active']) {
+	$active = $routes['home'];
 
 	/* Page info */
 	$page_title = 'Homepage';
-	$navigation = get_navigation($navigation_tpl, 0);
+	$navigation = get_navigation($navigation_tpl, $active['url']);
 
 	/* Page content */
 	$page_subtitle = "subtitle";
@@ -40,11 +53,12 @@ if (new_route('/', 'get')) {
 	/* Choose Template */
 	include use_template('main');
 } /* Instructions page */
-elseif (new_route('/instructions', 'get')) {
+elseif ($routes['instructions']['active']) {
+	$active = $routes['instructions'];
 
 	/* Page info */
 	$page_title = 'Pokemon instructions page';
-	$navigation = get_navigation($navigation_tpl, 1);
+	$navigation = get_navigation($navigation_tpl, 'instructions');
 
 	/* Page content */
 	$page_subtitle = 'very cool subtitle';
@@ -53,11 +67,12 @@ elseif (new_route('/instructions', 'get')) {
 	/* Choose Template */
 	include use_template('main');
 } /* Pokemon list page */
-elseif (new_route('/pokemon', 'get')) {
+elseif ($routes['pokemon']['active']) {
+	$active = $routes['pokemon'];
 
 	/* Page info */
 	$page_title = "Pokemon list page";
-	$navigation = get_navigation($navigation_tpl, 2);
+	$navigation = get_navigation($navigation_tpl, 'pokemon');
 
 	/* Page content */
 	$page_subtitle = 'subtitle';
@@ -67,7 +82,7 @@ elseif (new_route('/pokemon', 'get')) {
 	include use_template('main');
 
 } /* Pokemon handler API */
-elseif (new_route('/handler', 'post')) {
+elseif ($routes['handler']['active']) {
 
 	/* HIER DE FUNCTIES DIE DE ACTIES AFHANDELEN */
 } else {
