3dbbe9e0de01d8eaf6fd34f3292a645b34a8ceeb
VictorZwart <49715623+VictorZwart@users.noreply.github.com>
2019-05-29 12:47:41 +0200

    logo en begin button
    Logo erbij gezet en de button begonnen

--- views/main.php
+++ views/main.php
@@ -10,7 +10,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
        
         <!-- Own CSS -->
-        <link rel="stylesheet" href="../css/main.css">
+        <link rel="stylesheet" href="css/styles.css">
 
         <title><?= $page_title ?></title>
     </head>
@@ -20,22 +20,36 @@
 
         <!-- Content -->
         <div class="container">
-
+            <div class="row">
+                <div class="col-md-12">
+                    <figure>
+                        <img src="media/PHPokemon.png"
+                             alt=""
+                             width="900"
+                             height="124">
+                    </figure>
+                </div>
+            </div>
             <div class="row">
 
                 <!-- Left column -->
-                <div class="col-md-8">
+                <div class="col-md-10">
 
                     <h1><?= $page_title ?></h1>
                     <h5><?= $page_subtitle ?></h5>
                     <p><?= $page_content ?></p>
                     <?php if(isset($left_content)){echo $left_content;} ?>
+
+                    <!-- Button for playing -->
+                    <!-- todo: Styles in css zetten. Button mooier maken -->
+                    <div style="height:300px">
+                    <button type="button" class="btn btn-danger"
+                    style="width: 100%; height: 100%;" >
+                        Play!
+                    </button>
+                    </div>
                 </div>
 
-                <!-- Right column -->
-                <div class="col-md-4">
-                Mooie sidebar hier
-                </div>
             </div>
         </div>
     </body>
