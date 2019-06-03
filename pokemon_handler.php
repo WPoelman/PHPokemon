f0c249ac7f000ec0ba21a586d6d00c77b54dd9e3
Robin <robinvandernoord@gmail.com>
2019-06-03 19:52:36 +0200

    added get_profile handler for testing

--- pokemon_handler.php
+++ pokemon_handler.php
@@ -11,15 +11,27 @@ include 'model.php';
 function attack($info) {
 	echo 'attack stuff hier';  // = requests.post('http://localhost/eindproject/pokemon_handler.php/attack').text
 }
+
 $routes->new_route('attack', 'post');
 
 function start_game($info) {
 	$username = $_POST['username'];
-	$pokemon = explode(",", $_POST['pokemon']);
+	$pokemon  = explode(",", $_POST['pokemon']);
 
 	// dit is om te testen!
-	echo json_encode(["pokemon" => $pokemon, "username"=> $username]);
+	save_game_info($username, $pokemon);
+
+	echo json_encode(get_game_info());
 }
+
 $routes->new_route('start_game', 'post');
+
+function get_profile($info) {
+	echo json_encode(get_game_info());
+}
+
+$routes->new_route('get_profile', 'get');
+
+
 
 $routes->start();
