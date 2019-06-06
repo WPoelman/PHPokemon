06d55b7920bca9dccf938c37f81ae29a5f54e268
VictorZwart <49715623+VictorZwart@users.noreply.github.com>
2019-06-06 12:02:54 +0200

    Game over screen added
    Game over screen toegevoegd inclusief  game over image

--- views/main.php
+++ views/main.php
@@ -89,7 +89,7 @@
                 </div>
             </span>
     </div>
-
+    <br>
     <!--                    -->
     <!-- Pre game selection -->
     <!--       screen       -->
@@ -635,7 +635,7 @@
                 </div>
             </span>
     </div>
-
+    <br>
     <!--                  -->
     <!-- Main game screen -->
     <!--                  -->
@@ -891,7 +891,7 @@
                 </div>
             </span>
     </div>
-
+    <br>
     <!--                -->
     <!-- Waiting screen -->
     <!--                -->
@@ -969,6 +969,48 @@
             </div>
          </span>
     </div>
+
+    <!--                  -->
+    <!-- Game Over screen -->
+    <!--                  -->
+    <div class="row" id="game_over_screen">
+            <span class="border border-dark">
+                <div class="col-md-12 main-box">
+
+                    <div class="row justify-content-center">
+
+                        <!-- image for Game over screen -->
+                        <div class="col-md-6 text-center button-div">
+                        <img src="media/Game-over-screen.png">
+                    </div>
+                    </div>
+
+                    <!-- Middle text -->
+                    <div class="row">
+                        <hr>
+                        <div class="col-md-12">
+                            <p class="text-center Middle-Text">{insert player name} won! Congratulations!/Too bad!</p>
+                        </div>
+                        <hr>
+                    </div>
+                    <div class="wp-row justify-content-center">
+                        <div class="col-md-12 text-center">
+                            <h3>Try to pick a different team and try again:</h3>
+                        </div>
+                    </div>
+                    <!-- Return to home button -->
+                    <div class="row justify-content-center">
+
+                        <div class="col-md-4 text-center button-div ">
+                            <button type="button" class="btn btn-warning" id="return-home">
+                                <h2 class="ButtonText">Return <br> to home!</h2>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </span>
+    </div>
+
 </div>
 
 </body>
