d73de4a40779ca0fc876a42913b56214a33be687
frenkvdberg <flvdberg@gmail.com>
2019-06-09 15:56:59 +0200

    Logo aangepast qua hoogte en breedte en beter gecentreerd.

--- components/header.php
+++ components/header.php
@@ -39,7 +39,7 @@
 	<div class="row">
 		<div class="col-md-12">
 			<figure>
-				<img src="media/PHPokemon.png" alt="PHPokemon" width="60%" height="auto">
+				<img src="media/PHPokemon.png" alt="PHPokemon" width="auto" height="auto" id="PHPokemon_logo">
 			</figure>
 		</div>
 	</div>
